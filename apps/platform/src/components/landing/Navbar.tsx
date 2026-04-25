06e5ae8e0b331ba4266063b66601b6b49cefcc56
afsuyadi <afsuyadi@gmail.com>
2026-04-25 17:19:09 +0700

    patch(navbar): re-route button Login on Navbar

--- apps/platform/src/components/landing/Navbar.tsx
+++ apps/platform/src/components/landing/Navbar.tsx
@@ -1,13 +1,15 @@
+import { Link } from '@tanstack/react-router';
+
 export function Navbar() {
 	return (
 		<nav className="flex justify-between items-center px-6 py-4 shadow-sm bg-gray-50 ">
 			<span className="text-green-700 font-bold text-xl">Ihsanify</span>
-			<button
-				type="button"
+			<Link
+				to="/login"
 				className="bg-green-600 text-white px-8 py-3 rounded-full"
 			>
 				Login
-			</button>
+			</Link>
 		</nav>
 	);
 }
