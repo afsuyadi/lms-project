e273f18775ada7ebd5f591ca3941228e32d19a70
afsuyadi <afsuyadi@gmail.com>
2026-04-29 15:59:54 +0700

    patch(notification): add notification box popup

--- apps/platform/src/components/dashboard/Header.tsx
+++ apps/platform/src/components/dashboard/Header.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 export function Header() {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
 		<header className="flex flex-row justify-between p-3 bg-green-900 text-white">
 			<h1 className="font-bold">Dashboard</h1>
-			<div className="flex gap-7">
-				<span>🔔</span>
+			<div className="relative flex gap-7">
+				<button
+					type="button"
+					className="cursor-pointer"
+					onClick={() => setIsOpen(!isOpen)}
+				>
+					🔔
+				</button>
+				{isOpen && (
+					<div className="absolute right-0 top-8 z-50 text-white bg-gray-800 p-4">
+						Notification Panel
+					</div>
+				)}
 				<span>👤 Admin</span>
 			</div>
 		</header>
