6d43709dcba3f6f93e24feca9b22d35e7f24d903
afsuyadi <afsuyadi@gmail.com>
2026-04-29 18:00:11 +0700

    feat(notification): add Badge

--- apps/platform/src/components/dashboard/Header.tsx
+++ apps/platform/src/components/dashboard/Header.tsx
@@ -7,16 +7,20 @@ export function Header() {
 		<header className="flex flex-row justify-between p-3 bg-green-900 text-white">
 			<h1 className="font-bold">Dashboard</h1>
 			<div className="relative flex gap-7">
+				<span className="absolute -top-2 right-21 bg-red-500 rounded-full w-4 h-4 items-center flex font-bold justify-center text-xs">
+					3
+				</span>
 				<button
 					type="button"
-					className="cursor-pointer"
+					className="relative cursor-pointer"
 					onClick={() => setIsOpen(!isOpen)}
 				>
 					🔔
 				</button>
 				{isOpen && (
-					<div className="absolute right-0 top-8 z-50 text-white bg-gray-800 p-4">
-						Notification Panel
+					<div className="absolute right-1 top-8 z-50 text-white bg-gray-800 py-3 px-2 w-full rounded-lg">
+						<div>⚪ Quiz</div>
+						<div>⚪ Report</div>
 					</div>
 				)}
 				<span>👤 Admin</span>
