38b35836d081133ba1f3f74b081c848917a48140
afsuyadi <afsuyadi@gmail.com>
2026-05-08 18:43:03 +0700

    fix(mockAuth):: fix mockUser

--- apps/platform/src/lib/mockAuth.ts
+++ apps/platform/src/lib/mockAuth.ts
@@ -1,8 +1,13 @@
 export const mockUser: {
 	id: string;
-	teacherId: string | null
+	teacherId: string | null;
 	role: "admin" | "teacher" | "student";
 	name: string;
 } =
 	// { id:1, role: "admin", name: "Ahmad" };
-	{ id: "u-01", teacherId: "te-th-01", role: "teacher", name: "Ustadzah Lisna" };
+	{
+		id: "u-01",
+		teacherId: "te-th-01",
+		role: "teacher",
+		name: "Ustadzah Lisna",
+	};
